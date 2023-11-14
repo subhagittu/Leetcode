80fbc07af8885e3dc07c75b6179135c068a2a831
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-11-14 22:57:51 +0530

    Create 120.cpp

---
+++ 120.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp = triangle ;
+        for(int i = 0 ;i < n-1 ; i++){
+            for(int j = 0 ; j < dp[i].size(); j++){
+                dp[i][j] = 0 ;
+                cout<<dp[i][j]<<" "; 
+            }cout<<endl;
+        }
+        for(int i = n-2 ;i >=0 ; i--){
+            for(int j = 0 ; j < dp[i].size(); j++){
+                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j]; 
+            }
+        }
+        return dp[0][0] ;
+    }
+};
