72cfdf61e63a8010e84764851c2cbef04a08ea16
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-11 23:54:58 +0530

    Update 125.java

--- 125.java
+++ 125.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         if (s.isEmpty()) {
         	return true;
-        }
+        } 
         int start = 0;
         int last = s.length() - 1;
         while(start <= last) {
