9d1880bc01ce6bb6e37ceff61ff7f6d4544194d5
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-04-01 23:31:15 +0530

    Create 58.java

---
+++ 58.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+         s = s.trim(); // remove the spaces occuring at beginning and last
+        int n = s.length();
+        int count = 0;
+        for(int i=n-1;i>=0;i--){
+            if (s.charAt(i) == ' ') // if space encounter return from loop
+            {
+                return count;
+            }
+            else{
+                count++;
+            }
+        }
+        return count; // if string contains of only one word
+    }
+}
