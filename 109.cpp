8666f026ff82cd2e60d01e687d056466eb4776c3
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-05-09 21:23:27 +0530

    Update 109.cpp

--- 109.cpp
+++ 109.cpp
@@ -2,7 +2,7 @@ class Solution {
 public: 
     TreeNode* sortedListToBST(ListNode* head)  {
        
-        if(!head) return NULL;
+        if(!head) return NULL; 
         if(!head->next) return new TreeNode(head->val);
 
        
