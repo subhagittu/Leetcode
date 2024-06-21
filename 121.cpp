f0ca73059ed9bbbf922cfc6c1a490689865c291d
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-06-22 02:18:27 +0530

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     int maxProfit(vector<int>& prices) {
-        int min_price = prices[0];
+        int min_price = prices[0]; 
         int maxprof = 0;
  
         for(int i=1;i<prices.size();i++){
