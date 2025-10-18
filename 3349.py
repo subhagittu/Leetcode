4a52e598be865a890d2cf0f645e8e0b9fabd30e6
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-10-18 08:31:09 +0530

    Update 3349.py

--- 3349.py
+++ 3349.py
@@ -2,7 +2,7 @@ class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
         knew = k - 1
         if knew == 0:
-            return True 
+            return True
         for j in range(k + 1, len(nums)):
             if nums[j] > nums[j - 1] and nums[j - k] > nums[j - k - 1]:
                 knew -= 1
