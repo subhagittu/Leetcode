1d56aca626dd4de08893c9465baafad1375627a6
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-10-18 08:32:26 +0530

    Fix comparison logic in 3349.py

--- 3349.py
+++ 3349.py
@@ -4,7 +4,7 @@ class Solution:
         if knew == 0:
             return True
         for j in range(k + 1, len(nums)):
-            if nums[j] > nums[j - 1] and nums[j - k] > nums[j - k - 1]:
+            if nums[j] > nums[j - 1] and nums[j - k] > nums[j - k - 1]: 
                 knew -= 1
             else:
                 knew = k - 1
