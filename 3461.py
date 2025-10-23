fffee964d0320c1d2dcb32d0684ed6745b03336e
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-10-23 08:33:18 +0530

    Update 3461.py

--- 3461.py
+++ 3461.py
@@ -2,7 +2,7 @@ class Solution:
     def hasSameDigits(self, s: str) -> bool:
         s = list(s)
         length = len(s)
-        while length > 2:
+        while length > 2: 
             for i in range(length - 1):
                 s[i] = str((int(s[i]) + int(s[i+1])) % 10)
             length -= 1
