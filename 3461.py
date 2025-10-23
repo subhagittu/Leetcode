7638f49b2c533e8c3c248dc8ab5d858d86d64d9e
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-10-23 08:33:59 +0530

    Update 3461.py

--- 3461.py
+++ 3461.py
@@ -4,6 +4,6 @@ class Solution:
         length = len(s)
         while length > 2:
             for i in range(length - 1):
-                s[i] = str((int(s[i]) + int(s[i+1])) % 10)
+                s[i] = str((int(s[i]) + int(s[i+1])) % 10) 
             length -= 1
         return s[0] == s[1]
