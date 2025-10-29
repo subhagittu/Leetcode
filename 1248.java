29f1cff1318577248469cb996b8eefcc86d55436
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-10-29 08:54:01 +0530

    Update 1248.java

--- 1248.java
+++ 1248.java
@@ -2,7 +2,7 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] %= 2;
-        }
+        } 
         
         int[] prefixCount = new int[nums.length + 1];
         prefixCount[0] = 1;
