0092afb5aa9b3bc3f610ed13bc43bbc6226670ca
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-03 08:42:55 +0530

    Create 1578.cpp

---
+++ 1578.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0, max_time = neededTime[0], sum_time = neededTime[0];
+        for(int i = 1; i < neededTime.size(); i++) {
+            if(colors[i] != colors[i - 1]) {
+                ans += sum_time - max_time;
+                max_time = 0;
+                sum_time = 0;
+            }
+            max_time = max(max_time, neededTime[i]);
+            sum_time += neededTime[i];
+        }
+        ans += sum_time - max_time;
+        return ans;
+    }
+};
