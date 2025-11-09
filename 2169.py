65c6cfb1047bd8f8561a51b7581e9fbcbd286648
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-09 08:42:52 +0530

    Update 2169.py

--- 2169.py
+++ 2169.py
@@ -3,7 +3,7 @@ class Solution:
         def f(x, y, cnt):
             if x==0 or y==0: return cnt
             q,r=divmod(x, y)
-            return f(y, r, cnt+q)
+            return f(y, r, cnt+q) 
         return f(num1, num2, 0)
         
     
