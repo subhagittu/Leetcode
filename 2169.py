193fac99c9783a8c1067b603f2cfc86f49adb35f
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-09 08:39:08 +0530

    Create 2169.py

---
+++ 2169.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        def f(x, y, cnt):
+            if x==0 or y==0: return cnt
+            q,r=divmod(x, y)
+            return f(y, r, cnt+q)
+        return f(num1, num2, 0)
+        
