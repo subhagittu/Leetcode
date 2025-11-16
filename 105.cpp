5c50f5faea7f19e9b071588659018008f43275fa
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-16 08:53:23 +0530

    Update 105.cpp

--- 105.cpp
+++ 105.cpp
@@ -5,8 +5,7 @@ class Solution {
         }  
     }
 
-
-    TreeNode* build(vector<int> &pre, vector<int> &in, int &index, int inStart, int inEnd, int &n, unordered_map<int,int> &map){
+     TreeNode* build(vector<int> &pre, vector<int> &in, int &index, int inStart, int inEnd, int &n, unordered_map<int,int> &map){
         if(index>=n || inStart>inEnd) return NULL;
 
         int element = pre[index++];
