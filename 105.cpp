f60493fe05f8dc425f4215b4fe1d00001879d35c
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-16 08:54:19 +0530

    Update 105.cpp

--- 105.cpp
+++ 105.cpp
@@ -2,7 +2,7 @@ class Solution {
     void createMap(vector<int> &in, unordered_map<int,int> &map, int &n){ 
         for(int i=0; i<n; i++){  
             map[in[i]] = i;
-        }  
+        } 
     }
 
      TreeNode* build(vector<int> &pre, vector<int> &in, int &index, int inStart, int inEnd, int &n, unordered_map<int,int> &map){
