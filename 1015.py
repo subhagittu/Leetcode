9d159b97374ff67b9fabc640f2c61fa116503454
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-11-25 08:49:58 +0530

    Update 1015.py

--- 1015.py
+++ 1015.py
@@ -3,7 +3,7 @@ class Solution(object):
         if k % 2 == 0 or k % 5 == 0: 
             return -1
         rem = 1 % k 
-        len = 1
+        len = 1 
         while rem > 0:
             rem = (rem * 10 + 1) % k
             len += 1 
