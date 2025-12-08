f89de11913615f50277e1097a4f6810dea639f2e
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-08 08:19:33 +0530

    Update 1925.py

--- 1925.py
+++ 1925.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         sq = {i*i for i in range(1, n+1)} 
-        r = 0                               
+        r = 0                             
         
         
         for i in range(1, n+1):
