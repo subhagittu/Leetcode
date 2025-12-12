c5beb93942e1152faf837f07ca6556ce41633f54
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-12 12:05:40 +0530

    Update 3433.cpp

--- 3433.cpp
+++ 3433.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     vector<int> countMentions(int numberOfUsers, vector<vector<string>>& events) {
                
-        ranges::sort(events, {}, [](auto& e) {   
+        ranges::sort(events, {}, [](auto& e) {     
             return pair(stoi(e[1]), e[0][2]);   
         });  
 
