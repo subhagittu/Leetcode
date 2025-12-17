e51a72247ec78503fc0d434c1344941a4a1c7022
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-17 19:06:11 +0530

    Update 3573.py

--- 3573.py
+++ 3573.py
@@ -6,7 +6,7 @@ class Data:
 class Solution: 
     def maximumProfit(self, prices: List[int], k: int) -> int:
         x0=prices[0]
-        dp=[Data(0, -x0, x0) for _ in range(k+1)]
+        dp=[Data(0, -x0, x0) for _ in range(k+1)] 
         n=len(prices)
         for i in range(1, n):
             x=prices[i]
