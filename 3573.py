b4986c23f8b345e895da6ed1d687f9757ca284b9
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-17 19:05:21 +0530

    Update 3573.py

--- 3573.py
+++ 3573.py
@@ -8,7 +8,7 @@ class Solution:
         x0=prices[0]
         dp=[Data(0, -x0, x0) for _ in range(k+1)]
         n=len(prices)
-        for i in range(1, n): 
+        for i in range(1, n):
             x=prices[i]
             for t in range(k, 0, -1):
                 cur=dp[t]
