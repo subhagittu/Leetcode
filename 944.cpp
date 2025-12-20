072dfc5e1fd680d9fd84d0f2ccc2f194384a8435
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-20 09:05:52 +0530

    Update 944.cpp

--- 944.cpp
+++ 944.cpp
@@ -2,7 +2,7 @@ class Solution {
     bool isUnsorted(const vector<string>& strs, int j) {
         for (int i = 1; i < strs.size(); i++)
             if (strs[i][j] < strs[i - 1][j]) 
-                return true; 
+                return true;
         return false;  
     }  
 
