2770875d0d7cb995ee9e8ca79b785b8f0461d040
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-25 19:32:09 +0530

    Update 3075.py

--- 3075.py
+++ 3075.py
@@ -4,7 +4,7 @@ class Solution:
         christmasJoy.sort(reverse=True)
             
         totalJoy = 0      
-        for turn in range(gifts): 
+        for turn in range(gifts):
             currentJoy = christmasJoy[turn] - turn 
             if currentJoy <= 0:
                 break
