86e3e4528313ec23ff34bfab4fa0e26c2db4f2bf
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-28 10:49:27 +0530

    Update 1351.cpp

--- 1351.cpp
+++ 1351.cpp
@@ -1,7 +1,7 @@
 class Solution { 
 public:
     int countNegatives(vector<vector<int>>& grid) { 
-        int n=grid.size();   
+        int n=grid.size();  
         int m=grid[0].size(); 
         int sum=0;
         
