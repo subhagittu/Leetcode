9db5d8e29bf111bb61bddde9d8c19ff48baf58cb
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-29 19:10:14 +0530

    Update 756.cpp

--- 756.cpp
+++ 756.cpp
@@ -6,7 +6,7 @@ public:
     { 
         if(n==1) return true;    
              
-        if(ind==n-1)
+        if(ind==n-1) 
         { 
             if (dp.count(newlayer)) return dp[newlayer];
             return dp[newlayer]=solve(0,"",newlayer,n-1);
