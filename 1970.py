4c7886d17666f2eba14cef3a90e095416a151890
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-31 11:33:36 +0530

    Update 1970.py

--- 1970.py
+++ 1970.py
@@ -2,7 +2,7 @@ class Solution(object):
     def latestDayToCross(self, row, col, cells):
         """   
         :type row: int    
-        :type col: int       
+        :type col: int        
         :type cells: List[List[int]]  
         :rtype: int
         """
