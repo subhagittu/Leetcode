e535b59687cfffdb249a0352797b0169f95e4bc7
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2025-12-31 11:30:56 +0530

    Create 1970.py

---
+++ 1970.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def latestDayToCross(self, row, col, cells):
+        """
+        :type row: int
+        :type col: int
+        :type cells: List[List[int]]
+        :rtype: int
+        """
+        n = row * col
+        top, bottom = n, n + 1
+        parent = list(range(n + 2))
+        rank = [0] * (n + 2)
+        land = [False] * n
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def unite(a, b):
+            a, b = find(a), find(b)
+            if a == b: return
+            if rank[a] < rank[b]: parent[a] = b
+            elif rank[a] > rank[b]: parent[b] = a
+            else: parent[b] = a; rank[a] += 1
+
+        for d in range(len(cells) - 1, -1, -1):
+            r, c = cells[d][0] - 1, cells[d][1] - 1
+            id = r * col + c
+            land[id] = True
+
+            for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col:
+                    nid = nr * col + nc
+                    if land[nid]:
+                        unite(id, nid)
+
+            if r == 0: unite(id, top)
+            if r == row - 1: unite(id, bottom)
+
+            if find(top) == find(bottom):
+                return d
+        return 0
