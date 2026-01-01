1403fa81f55a4fb57102dd9b0a52f97aa213d121
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-01 09:28:30 +0530

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        s1 = ''
+        for i in digits:
+            s1 += str(i)
+        s1 = int(s1)
+        s2 = s1+1
+        s2 = str(s2)
+        l1 = []
+        for i in s2:
+            l1.append(int(i))
+        return l1
+        
