fac936bb258e946fc4cd13137134969aa192ab30
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-04 11:12:18 +0530

    Update 1390.py

--- 1390.py
+++ 1390.py
@@ -23,6 +23,40 @@ class Solution(object):
             if len(l2) == 4:
                 sum1 += sum(l2)
 
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Optimized one - 
+
+
+class Solution:
+    def sumFourDivisors(self, nums):
+        total = 0
+        for n in nums:
+            root1 = int(n**0.5)
+            sum1 = 0
+            cnt = 0
+            for i in range(1,root1+1):
+                if n%i==0:
+                    j = n//i
+                
+                    if i == j:
+                        sum1 += i
+                        cnt += 1
+                    else:
+                        cnt += 2
+                        sum1 += i + j
+
+                if cnt > 4:
+                    break
+            if cnt == 4:
+                total += sum1
+        return total
+
+
+
+        
         
             
 
