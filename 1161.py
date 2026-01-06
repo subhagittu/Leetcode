ae55d0b07e735656ecbee7aa829aabbe9ec9fd35
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-06 16:02:26 +0530

    Update 1161.py

--- 1161.py
+++ 1161.py
@@ -2,7 +2,7 @@
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val 
-        self.left = left  
+        self.left = left
         self.right = right 
 class Solution(object):
     def maxLevelSum(self, root):
