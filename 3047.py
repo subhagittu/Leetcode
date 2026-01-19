969472db5f2859ea9c4b1dc06d8fd0eb9101c9db
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-19 08:21:14 +0530

    Create 3047.py

---
+++ 3047.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def largestSquareArea(self, bottomLeft, topRight):
+        """
+        :type bottomLeft: List[List[int]]
+        :type topRight: List[List[int]]
+        :rtype: int
+        """
+        max_size = 0
+
+        for (bottom_left_i, top_right_i), (
+            bottom_left_j,
+            top_right_j,
+        ) in combinations(zip(bottomLeft, topRight), 2):
+            w = min(top_right_i[0], top_right_j[0]) - max(
+                bottom_left_i[0], bottom_left_j[0]
+            )
+            h = min(top_right_i[1], top_right_j[1]) - max(
+                bottom_left_i[1], bottom_left_j[1]
+            )
+
+            max_size = max(max_size, min(w, h))
+
+        return max_size * max_size
