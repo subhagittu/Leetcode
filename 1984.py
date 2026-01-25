5b3b5feed6eeced7ed73ce7efa4c4e69823e2f12
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-25 10:53:12 +0530

    Create 1984.py

---
+++ 1984.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k == 1:
+            return 0
+
+        nums.sort()
+        
+        min_diff = float('inf')
+        
+        for i in range(len(nums) - k + 1):
+            current_diff = nums[i + k - 1] - nums[i]
+            min_diff = min(min_diff, current_diff)
+            
+        return min_diff
+        
