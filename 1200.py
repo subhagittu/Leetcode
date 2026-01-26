030d07abf429c5d6287ea93d4fcf4f9aa6fa45e6
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-01-26 09:15:03 +0530

    Create 1200.py

---
+++ 1200.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minimumAbsDifference(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[List[int]]
+        """
+        arr.sort()
+        ans = []
+        min_diff = float('inf')
+        for i in range(1,len(arr)):
+            if arr[i] - arr[i-1] < min_diff:
+                min_diff = arr[i] - arr[i-1]
+
+        for i in range(1,len(arr)):
+            if arr[i] - arr[i-1] == min_diff:
+                ans.append([arr[i-1],arr[i]])
+        return ans
+
+                
