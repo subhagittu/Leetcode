0f9adb1d820a606b0691f7421dd1e4666724b155
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-02-09 19:42:39 +0530

    Create 110.py

---
+++ 110.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        self.balanced = True
+        def dfs(node):
+            if node is None:
+                return 0
+
+            l = dfs(node.left)
+            r = dfs(node.right)
+
+            if abs(l - r) > 1:
+                self.balanced = False
+
+            return max(l, r) + 1
+
+        dfs(root)
+        return self.balanced
