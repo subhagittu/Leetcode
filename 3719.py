5925422c6b5876a5810c335f1a5b577f3e2e6857
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-02-10 09:33:57 +0530

    Add longestBalanced method to Solution class
    Implement a method to find the longest balanced subarray of 0s and 1s.

---
+++ 3719.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestBalanced(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        res = 0
+
+        seen = [0] * (max(nums) + 1)
+
+        for i in range(n):
+            A = [0, 0]
+
+            for j in range(i, n):
+                val = nums[j]
+                if seen[val] != i + 1:
+                    seen[val] = i + 1
+                    A[val & 1] += 1
+
+                if A[0] == A[1]:
+                    res = max(res, j - i + 1)
+
+        return res
