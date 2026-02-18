46968ebdfe9ba56cc17d27fdb9aa53aa7a897f38
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-02-18 23:50:37 +0530

    Create 560.py

---
+++ 560.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sub_num = {0:1}
+        total = count = 0
+
+        for n in nums:
+            total += n
+            
+            if total - k in sub_num:
+                count += sub_num[total-k]
+            if total in sub_num:
+                sub_num[total] += 1
+            else:
+                sub_num[total] = 1
+            #sub_num[total] = 1 + sub_num.get(total, 0)
+        
+        return count
+        
