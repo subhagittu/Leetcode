986c36f4b1e699842926c72d9c36dca45c0c4e4b
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-02-23 08:28:49 +0530

    Create 1461.py

---
+++ 1461.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hasAllCodes(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        st = set()
+
+        for i in range(len(s) - k + 1):
+            st.add(s[i:i+k])
+
+        return len(st) == 2**k
