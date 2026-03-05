990b8a5611033a2b318cfc41b1b79578ef669fa2
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-03-05 09:21:16 +0530

    Create 1758.py

---
+++ 1758.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mismatchA = 0  # vs "0101..."
+        mismatchB = 0  # vs "1010..."
+
+        for i, ch in enumerate(s):
+            expA = '0' if i % 2 == 0 else '1'
+            if ch != expA:
+                mismatchA += 1
+            else:
+                mismatchB += 1
+        return min(mismatchA, mismatchB)
