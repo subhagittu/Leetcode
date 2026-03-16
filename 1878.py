38a455c0c8a0b68427c61a14e3e3de811a61dcb4
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-03-16 09:05:32 +0530

    Create 1878.py

---
+++ 1878.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def getBiggestThree(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[int]
+        """
+        m, n = len(grid), len(grid[0])
+        s = set()
+
+        for i in range(m):
+            for j in range(n):
+                s.add(grid[i][j])
+
+                k = 1
+                while True:
+                    if i-k<0 or i+k>=m or j-k<0 or j+k>=n:
+                        break
+
+                    total = 0
+
+                    r, c = i-k, j
+                    for t in range(k):
+                        total += grid[r+t][c+t]
+
+                    r, c = i, j+k
+                    for t in range(k):
+                        total += grid[r+t][c-t]
+
+                    r, c = i+k, j
+                    for t in range(k):
+                        total += grid[r-t][c-t]
+
+                    r, c = i, j-k
+                    for t in range(k):
+                        total += grid[r-t][c+t]
+
+                    s.add(total)
+                    k += 1
+
+        return sorted(s, reverse=True)[:3]
