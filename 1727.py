d3b1d1147c13bb7bf5234cc31e36cd9e4ff7380e
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-03-17 08:58:23 +0530

    Create 1727.py

---
+++ 1727.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestSubmatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        res = 0
+
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i - 1][j]
+
+        for i in range(m):
+            matrix[i].sort(reverse=True)
+            for j in range(n):
+                res = max(res, matrix[i][j] * (j + 1))
+
+        return res
