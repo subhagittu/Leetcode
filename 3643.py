6f557642e2a06dac0e212717ce3ee36a201adc64
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-03-21 10:12:02 +0530

    Create 3643.py

---
+++ 3643.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseSubmatrix(self, grid, x, y, k):
+        """
+        :type grid: List[List[int]]
+        :type x: int
+        :type y: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        for i in range(k >> 1):
+            row1 = grid[x + i]
+            row2 = grid[x + k - 1 - i]
+            for j in range(k):
+                row1[y + j], row2[y + j] = row2[y + j], row1[y + j]
+        
+        return grid
+        
