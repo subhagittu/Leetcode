e4d008ef4a2973472a3e478b226b1dad9ccf1578
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-03-22 17:29:24 +0530

    Update 1886.py

--- 1886.py
+++ 1886.py
@@ -1,4 +1,14 @@
 class Solution(object):
+    def findtranspose(self, mat):
+        rows = len(mat)
+        cols = len(mat[0])
+        transpose1 = [[0]*rows for _ in range(cols)]
+        for i in range(0,len(mat)):
+            for j in range(0,len(mat[i])):
+                transpose1[j][i] = mat[i][j]
+
+        return transpose1
+
     def findRotation(self, mat, target):
         """
         :type mat: List[List[int]]
@@ -9,5 +19,7 @@ class Solution(object):
             if mat == target:
                 return True
             # Rotate 90 degrees clockwise
-            mat = [list(row) for row in zip(*mat[::-1])]
+            mat = mat[::-1]
+            mat = self.findtranspose(mat)
+            #mat = [list(row) for row in zip(*mat[::-1])]
         return False
