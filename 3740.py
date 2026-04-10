07864a2510f527c97d7f45e4803d8c2d33b0cda1
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-04-10 09:26:12 +0530

    Create 3740.py

---
+++ 3740.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        ans = n + 1
+
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if nums[i] != nums[j]:
+                    continue
+                for k in range(j + 1, n):
+                    if nums[j] == nums[k]:
+                        ans = min(ans, k - i)
+                        break
+
+        return -1 if ans == n + 1 else ans * 2
+        
