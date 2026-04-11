65edd8df32c4386d018035acfecd805f077285d0
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-04-11 09:52:17 +0530

    Create 3741.py

---
+++ 3741.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n, M=len(nums), max(nums)
+        pos=[(-1, -1) for _ in range(M+1)]
+        ans=1<<32
+        for k, x in enumerate(nums):
+            if pos[x][1]!=-1:
+                ans=min(ans, (k-pos[x][1])<<1)
+            pos[x]=k, pos[x][0]
+        return -1 if ans==1<<32 else ans      
