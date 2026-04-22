8468328ce6e7a0514be8cc097d9f4ed3fb89e49d
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-04-22 09:31:54 +0530

    Create 2452.py

---
+++ 2452.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def twoEditWords(self, queries, dictionary):
+        """
+        :type queries: List[str]
+        :type dictionary: List[str]
+        :rtype: List[str]
+        """
+        ans = []
+
+        for q in queries:
+            for d in dictionary:
+                diff = 0
+
+                for i in range(len(q)):
+                    if q[i] != d[i]:
+                        diff += 1
+
+                    if diff > 2:
+                        break
+
+                if diff <= 2:
+                    ans.append(q)
+                    break
+
+        return ans
+        
