7e81105992ad39c6535e949f00b45038b35792b1
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-04-26 21:36:17 +0530

    Create 1559.py

---
+++ 1559.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def containsCycle(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: bool
+        """
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for r in range(m):
+            for c in range(n):
+                if visited[r][c]:
+                    continue
+
+                
+                stack = [(r, c, -1, -1)]
+                visited[r][c] = True
+
+                while stack:
+                    cr, cc, pr, pc = stack.pop()
+
+                    for dr, dc in dirs:
+                        nr, nc = cr + dr, cc + dc
+
+                        if nr < 0 or nr >= m or nc < 0 or nc >= n:
+                            continue
+                        if grid[nr][nc] != grid[cr][cc]:
+                            continue
+                        if nr == pr and nc == pc:
+                            continue
+
+                        if visited[nr][nc]:
+                            return True
+
+                        visited[nr][nc] = True
+                        stack.append((nr, nc, cr, cc))
+
+        return False
