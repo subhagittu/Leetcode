b2fe1af91ab3afd4e854bdea182ecba6a09c9433
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-05-06 12:31:55 +0530

    Create 1681.py

---
+++ 1681.py
@@ -0,0 +1,24 @@
+import copy
+class Solution(object):
+    def rotateTheBox(self, boxGrid):
+        """
+        :type boxGrid: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        grid = copy.deepcopy(boxGrid)
+        rows, cols = len(grid), len(grid[0])
+        for r in range(rows):
+            p = 0
+            for c in range(cols):
+                if grid[r][c] == '.':
+                    grid[r][c], grid[r][p] = grid[r][p], grid[r][c]
+                    p += 1
+                elif grid[r][c] == '*':
+                    p = c + 1
+        
+        res = [[''] * rows for _ in range(cols)]
+        for r in range(rows):
+            for c in range(cols):
+                res[c][rows - 1 - r] = grid[r][c]
+                
+        return res
