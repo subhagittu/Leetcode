b93153a2d0ea4e60d47a7c44c70fb30160f4c306
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-06-08 23:27:10 +0530

    Create 2161.py

---
+++ 2161.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def pivotArray(self, nums, pivot):
+        """
+        :type nums: List[int]
+        :type pivot: int
+        :rtype: List[int]
+        """
+        n=len(nums)
+        if n==1: return nums
+        R=[]
+        l, m=0, 0
+        for x in nums:
+            if x<pivot:
+                nums[l]=x
+                l+=1
+            elif x>pivot:
+                R.append(x)
+            else:
+                m+=1
+        return nums[:l]+[pivot]*m+R
+        
