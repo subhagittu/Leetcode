77a81480c4513f577e50cdc6a726b080e88c2a38
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-06-21 17:24:25 +0530

    Update 1018.py

--- 1018.py
+++ 1018.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def prefixesDivBy5(self, nums):
-        cur = 0  
+        cur = 0 
         res = []
         for bit in nums:
             cur = ((cur << 1) + bit) % 5 
