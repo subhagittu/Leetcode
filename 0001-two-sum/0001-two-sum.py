876a43765e0e69cb867c641ecc3054015f4e6fa9
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-06-23 20:57:56 +0530

    Time: 759 ms (42.08%), Space: 12.7 MB (98.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i,ele in enumerate(nums):
+            if target-ele in nums:
+                index1 = nums.index(target-ele)
+                if i != index1:
+                    return [i,index1]
+
+        
