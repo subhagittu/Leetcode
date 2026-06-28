ee8b2aefebb6faa9ba3e49372f4472e1c0c0201d
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-06-28 22:43:29 +0530

    Time: 77 ms (5.26%), Space: 21.4 MB (5.26%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: int
         """
         n = len(arr)
-        freq = [0] * (n + 1)
-
+        freq = [0]*(n+1)
         for x in arr:
-            freq[min(x, n)] += 1
-
+            freq[min(x,n)] += 1
         res = 1
-        for i in range(2, n + 1):
-            res = min(res + freq[i], i)
+        for i in range(2,n+1):
+            res = min(res+freq[i], i)
 
         return res
+
