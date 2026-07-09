0ac6f2b528785b8f630298e6d49ec44dc0bacb59
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-09 09:13:25 +0530

    Time: 119 ms (54.55%), Space: 45.3 MB (27.27%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+        """
+        :type n: int
+        :type nums: List[int]
+        :type maxDiff: int
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
+        comp = [0] * n
+        res = []
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] <= maxDiff:
+                comp[i] = comp[i - 1]
+            else:
+                comp[i] = comp[i - 1] + 1
+        for u,v in queries:
+            if comp[u] == comp[v]:
+                res.append(True)
+            else:
+                res.append(False)
+
+        return res
+        #return [comp[u] == comp[v] for u, v in queries]
