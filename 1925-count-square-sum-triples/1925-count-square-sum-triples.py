d9e843ee85c088f525ce8f80048fb142b9706586
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-10 09:19:06 +0530

    Time: 64 ms (88.46%), Space: 12.2 MB (91.03%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countTriples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sq = {i*i for i in range(1, n+1)}  
+        r = 0                               
+        
+        
+        for i in range(1, n+1):
+            temp = i*i                      
+            for j in range(i+1, n+1):
+                s = temp + j*j              
+                
+                
+                if s in sq:
+                    r += 2                 
+        
+        return r
