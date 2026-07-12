a4099afba8d3ce8378369a73bbc58e77688c10fd
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-12 09:44:11 +0530

    Time: 43 ms (73.66%), Space: 28.8 MB (36.63%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -6,7 +6,11 @@ class Solution(object):
         """
         ranks = {}
         rank = 1
-        for x in sorted(list(set(arr))):
+        arr1 = []
+        arr1[:] = set(arr)
+        arr1[:] = list(arr1)
+        arr1.sort()
+        for x in arr1:
             ranks[x] = rank
             rank += 1
         for i in range(len(arr)):
