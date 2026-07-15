3c3a32872e8ef2608837573e3e8da45ad78a96ab
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-15 09:24:27 +0530

    Time: 31 ms (24.5%), Space: 12.2 MB (92.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -6,24 +6,20 @@ class Solution(object):
         :rtype: int
         """
         odd1 = 1
+        even1 = 2
         sumodd = 0
+        sumeven = 0
 
-        for i in range(n):
+        for i in range(0,n):
             sumodd += odd1
             odd1 += 2
 
-        even1 = 2
-        sumeven = 0
-
-        for i in range(n):
+        for i in range(0,n):
             sumeven += even1
             even1 += 2
+
         while sumeven != 0:
             sumodd, sumeven = sumeven, sumodd % sumeven
 
         return sumodd
 
-
-
-        
-        
