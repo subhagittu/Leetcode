599af16390a6beb9c8975cd352c0ce4e9637cddf
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-19 21:44:39 +0530

    Create 1081.py

---
+++ 1081.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        freq = Counter(s)
+        seen = set()
+        stack = []
+
+        for c in s:
+            freq[c] -= 1
+            if c in seen: continue
+
+            while stack and stack[-1] > c and freq[stack[-1]]:
+                seen.remove(stack.pop())
+
+            stack.append(c)
+            seen.add(c)
+
+        return "".join(stack)
+        
