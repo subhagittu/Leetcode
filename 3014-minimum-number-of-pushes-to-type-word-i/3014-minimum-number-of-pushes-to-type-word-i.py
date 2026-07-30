36bfed0ae1d346e77ea1a607de288a8acc86f497
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-07-30 11:56:42 +0530

    Time: 0 ms (100%), Space: 12.4 MB (32.69%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -8,5 +8,12 @@ class Solution(object):
 
         fullGroups = n // 8
         remaining = n % 8
+        res = 0
+        num1 = 1
+        while num1 <= fullGroups:
+            res += num1 * 8
+            num1 += 1
 
-        return (fullGroups * 4 + remaining) * (fullGroups + 1)
+        res += remaining*num1
+
+        return res
