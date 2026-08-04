9f6d4e9604e07175a45a7f605690e02be7a75888
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-08-04 07:57:24 +0530

    Time: 4 ms (73.5%), Space: 12.2 MB (87.79%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -8,9 +8,11 @@ class Solution(object):
         minum = min(nums)
         l1 = []
         while minum <= maxnum:
-            if minum not in nums:
+            if minum in nums:
+                minum += 1
+                continue
+            else:
                 l1.append(minum)
-            
             minum += 1
 
         return l1
