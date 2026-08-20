8a349ba5e19aebb3ed1cc540c169dc82aacc9da3
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-08-20 10:02:48 +0530

    Time: 58 ms (67.16%), Space: 16.8 MB (53.73%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -7,22 +7,23 @@ class Solution(object):
         """
         seats = defaultdict(set)
 
-        for row, seat in reservedSeats:
-            if seat in [2, 3, 4, 5]:
+        for row,seat in reservedSeats:
+            if seat in [2,3,4,5]:
                 seats[row].add(0)
-            
-            if seat in [4, 5, 6, 7]:
-                seats[row].add(1)
-            
-            if seat in [6, 7, 8, 9]:
-                seats[row].add(2)
-        
-        total = 2 * n
 
+            if seat in [4,5,6,7]:
+                seats[row].add(1)
+
+            if seat in [6,7,8,9]:
+                seats[row].add(2)
+
+
+        total = 2*n
         for i in seats:
             if len(seats[i]) == 3:
-                total-= 2
+                total -= 2
             else:
-                total-= 1
-        
+                total -= 1
+
         return total
+
