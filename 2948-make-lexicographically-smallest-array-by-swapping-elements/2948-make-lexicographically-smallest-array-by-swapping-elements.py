6d86ddd17d72d9f3ea679a22f9673882330811b7
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-08-29 23:05:05 +0530

    Time: 296 ms (100%), Space: 48.9 MB (23.08%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lexicographicallySmallestArray(self, A, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: List[int]
+        """
+        groups = []
+        gmap = {}
+
+        for val in sorted(A):
+            if not groups or val - groups[-1][-1] > limit:
+                groups.append([])
+            groups[-1].append(val)
+            gmap[val] = len(groups) - 1
+
+        itr = [iter(g) for g in groups]
+
+        for i in range(len(A)):
+            A[i] = next(itr[gmap[A[i]]])
+
+        return A
