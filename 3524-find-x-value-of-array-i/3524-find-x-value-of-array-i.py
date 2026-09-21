99280b686bc0a317c2b67e46ed204e49d982bef0
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-09-21 19:56:39 +0530

    Time: 516 ms (66.67%), Space: 21.5 MB (0%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def resultArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        result = [0] * k
+        state = [0] * k
+        for value in nums:
+            rem = value % k
+            next_state = [0] * k
+            for r in range(k):
+                product_rem = (r * rem) % k
+                next_state[product_rem] += state[r]
+                result[product_rem] += state[r]
+            next_state[rem] += 1
+            result[rem] += 1
+            state = next_state
+        return result
