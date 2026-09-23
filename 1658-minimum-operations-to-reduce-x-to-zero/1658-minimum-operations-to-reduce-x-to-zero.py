4783434c1e3e29311f76547de790a8eff143f6e2
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2026-09-23 23:26:03 +0530

    Time: 109 ms (56.49%), Space: 20.2 MB (92.86%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minOperations(self, A, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        k = sum(A) - x
+        if k < 0: return -1 
+        best = -1
+        
+        s = i = 0
+        
+        for j, num in enumerate(A):
+            s += num
+            while s > k:
+                s -= A[i]
+                i += 1  
+            if s == k:
+                best = max(best, j - i + 1)
+
+        return -1 if best < 0 else len(A) - best
